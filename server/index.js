01d82d130e3f55f4ee3e1fb36bd1651804c69688
Sergio <sergio23jr@gmail.com>
2023-03-18 22:49:51 -0500

    mongoDB setup

--- server/index.js
+++ server/index.js
@@ -13,3 +13,13 @@ app.get("/api", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://sergio23jr:Soccer2394@to-do-app.w4pn1rb.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
